47071db86ae62bb61a26ea04d7788ea5861eba1b
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2024-01-18 10:52:25 +1000

    first success

--- first.py
+++ first.py
@@ -1,12 +1,10 @@
 import os
-import json
 from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = OpenAI(
-    # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
@@ -19,3 +17,5 @@ chat_completion = client.chat.completions.create(
     ],
     model="gpt-3.5-turbo",
 )
+
+print(chat_completion.choices[0].message)
